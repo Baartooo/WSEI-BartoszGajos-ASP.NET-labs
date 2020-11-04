eadabdaf2be7a3c0afb1a2f94c33add13fdfedb2
Baartooo <bartek.gajos19@gmail.com>
2020-11-04 22:47:35 +0100

    kinda stuck

---
+++ NewBrandingStyle.Web/Controllers/AddingController.cs
@@ -0,0 +1,21 @@
+﻿using ExchangeThings.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExchangeThings.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AddingController : ControllerBase
+    {
+
+        public IActionResult Post(ItemModel item)
+        {
+            //what should I return?
+        }
+
+    }
+}
---
+++ NewBrandingStyle.Web/wwwroot/js/addNewItem.js
@@ -0,0 +1,40 @@
+﻿(function () {
+    const alertElement = document.querySelector("#success-alert");
+    const formElement = document.forms[0];
+
+    const addNewItem = async () => {
+        const Name = document.querySelector('#Name').value;
+        const Description = document.querySelector('#Description').value;
+        const IsVisible = document.querySelector('#IsVisible').value;
+
+        const requestData = {
+            Name,
+            Description,
+            IsVisible
+        }
+        console.log(requestData);
+
+        const response = await fetch('api/Adding', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(requestData),
+        });
+
+        const responseJson = await response.json();
+
+        if (responseJson.success) {
+            alertElement.style.display = 'block'
+        }
+    };
+
+    window.addEventListener('load', () => {
+        formElement.addEventListener('submit', e => {
+            e.preventDefault();
+
+            addNewItem().then((res) => console.log(res));
+        })
+    })
+
+})();
