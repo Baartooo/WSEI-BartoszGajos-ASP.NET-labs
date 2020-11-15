47aaa6a7b408fa2398bdc8aad8bd77d3eed7e9e8
Baartooo <bartek.gajos19@gmail.com>
2020-11-15 16:41:39 +0100

    implemented adding item to DB

--- NewBrandingStyle.Web/Controllers/AddingController.cs
+++ NewBrandingStyle.Web/Controllers/AddingController.cs
@@ -1,4 +1,6 @@
-﻿using ExchangeThings.Web.Models;
+﻿using ExchangeThings.Web.Database;
+using ExchangeThings.Web.Entities;
+using ExchangeThings.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,8 +13,26 @@ namespace ExchangeThings.Web.Controllers
     [Route("api/[controller]")]
     public class AddingController : ControllerBase
     {
+
+        private readonly ExchangesDbContext _dbContext;
+
+        public AddingController(ExchangesDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public AddNewItemResponse Post(ItemModel item)
         {
+            var entity = new ItemEntity
+            {
+                Name = item.Name,
+                Description = item.Description,
+                IsVisible = item.IsVisible,
+            };
+
+            _dbContext.Items.Add(entity);
+            _dbContext.SaveChanges();
+
             var response = new AddNewItemResponse()
             {
                 success = true,
